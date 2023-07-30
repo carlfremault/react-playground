9c052fbf6b560d5f67ce0e90b0fab473323e179d
Carl Fremault <carlfremault@yahoo.com>
2023-07-30 11:46:27 +0200

    -more content - reorganised spacing - added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div
+      id="footer"
+      className="d-flex justify-content-center align-items-center"
+    >
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -72,65 +72,103 @@ const Home = () => {
         </Container>
       </section>
       {/* First section: image - text (wide) */}
-      <section id="first-section" className="mb-5">
-        <div className="mb-5">
-          <Row style={{ margin: '0', padding: '0' }}>
+      <Container fluid className="p-0">
+        <section className="wide-section">
+          <Row>
             <Col lg="6" className="p-0">
               <img src="/images/neom-4K_x--J2Jbc-unsplash.jpg" />
             </Col>
             <Col
               lg="6"
-              className="d-flex justify-content-center align-items-center p-3"
+              className="d-flex flex-column justify-content-center align-items-center p-3 p-sm-5 px-lg-0"
             >
-              <div className="text-container p-0 p-sm-5 p-lg-0">
+              <div className="text-container">
                 <h2 className="mb-4">First Section Here</h2>
-                <p className="">{lorem_50}</p>
+                <p className="">{lorem_40}</p>
                 <p>{lorem_20}</p>
               </div>
             </Col>
           </Row>
-        </div>
-      </section>
+        </section>
+      </Container>
       {/* Second section: title - text */}
-      <section className="mb-5">
-        <Container fluid="xl" className="content-container">
-          <Row style={{ margin: '0', padding: '0' }}>
-            <Col lg="6">
-              <h2 className="px-sm-5 mb-4 ms-lg-5">
-                Second Section With Some Explanation
-              </h2>
+      <Container fluid="xl" className="p-0">
+        <section className="section-container">
+          <Row className="p-3 p-sm-5">
+            <Col lg="6" className="p-0">
+              <h2 className="mb-4">Second Section With Some Explanation</h2>
             </Col>
-            <Col lg="6">
-              <div className="text-container p-0 px-sm-5 p-lg-0">
-                <p>{lorem_15}</p>
-                <p>{lorem_10}</p>
-                <ul>
-                  <li>{lorem_20}</li>
-                  <li>{lorem_5}</li>
-                  <li>{lorem_10}</li>
-                </ul>
-                <p>{lorem_15}</p>
+            <Col lg="6" className="p-0">
+              <p>{lorem_15}</p>
+              <p>{lorem_10}</p>
+              <ul>
+                <li>{lorem_20}</li>
+                <li>{lorem_5}</li>
+                <li>{lorem_10}</li>
+              </ul>
+              <p>{lorem_15}</p>
+            </Col>
+          </Row>
+        </section>
+      </Container>
+      {/* Wide image */}
+      <Container fluid className="p-0">
+        <img
+          src="/images/neom-9E9NsEiUGxg-unsplash 1920x1280.jpg"
+          className="wide-image"
+        />
+      </Container>
+      {/* Third section: title - text */}
+      <Container fluid="lg" className="p-0">
+        <section className="section-container">
+          <Row className="p-3 p-sm-5">
+            <Col lg="6" className="p-0">
+              <h2 className="mb-4">Third Section, more stuff</h2>
+            </Col>
+            <Col lg="6" className="p-0">
+              <ul>
+                <li>{lorem_35}</li>
+                <li>{lorem_25}</li>
+                <li>{lorem_10}</li>
+              </ul>
+            </Col>
+          </Row>
+        </section>
+      </Container>
+      {/* Foruth section: text - image (wide) */}
+      <Container fluid className="p-0">
+        <section className="wide-section">
+          <Row>
+            <Col lg={{ size: 6, order: 2 }} className="vertical-image p-0">
+              <img src="/images/neom-dulVtESluoM-unsplash 1920x2879.jpg" />
+            </Col>
+            <Col
+              lg={{ size: 6, order: 1 }}
+              className="d-flex flex-column justify-content-center align-items-center p-3 p-sm-5 px-lg-0"
+            >
+              <div className="text-container">
+                <h2 className="mb-4">One more section ...</h2>
+                <p className="">{lorem_30}</p>
+                <p>{lorem_20}</p>
               </div>
             </Col>
           </Row>
-        </Container>
-      </section>
-      <section>
-        <Container fluid="lg" className="pt-4 pt-lg-0 mb-5">
-          <Row>
-            <Col lg="8" className="p-0">
-              <img
-                id="p1-image"
-                className="img-fluid"
-                src="/images/neom-4K_x--J2Jbc-unsplash.jpg"
-              />
+        </section>
+      </Container>
+      {/* Fifth section: title - text */}
+      <Container fluid="lg" className="p-0">
+        <section className="section-container">
+          <Row className="p-3 p-sm-5">
+            <Col lg="6" className="p-0">
+              <h2 className="mb-4">Last section, conclusion</h2>
             </Col>
-            <Col lg="4" className="p-3">
-              <p className="p-3">{lorem_35}</p>
+            <Col lg="6" className="p-0">
+              <p>{lorem_45}</p>
+              <p>{lorem_50}</p>
             </Col>
           </Row>
-        </Container>
-      </section>
+        </section>
+      </Container>
     </>
   );
 };
